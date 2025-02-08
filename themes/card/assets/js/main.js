f7815481eb474b9e6919ee870fbcd362e5136160
Angel Crawford <16148683+AngelCrawford@users.noreply.github.com>
2025-02-08 21:38:14 +0100

    Started with the JS Events, #58

--- themes/card/assets/js/main.js
+++ themes/card/assets/js/main.js
@@ -93,6 +93,38 @@ function eraseCookie(name) {
   document.cookie = name+'=; Max-Age=-99999999;';
 };
 
+
+// ********************************************* EVENTS *********************************************
+    
+var dateObject = new Date();
+var month = dateObject.getMonth() + 1;
+var day = dateObject.getDate();
+var nowDate = dateObject.getFullYear() + "-" + (month < 10 ? '0' : '') + month + "-" + (day < 10 ? '0' : '') + day;
+
+// var xmasStart = dateObject.getFullYear() + "-12-01";
+// var xmasEnd = dateObject.getFullYear() + "-12-27";
+var xmasStart = dateObject.getFullYear() + "-02-01";
+var xmasEnd = dateObject.getFullYear() + "-02-27";
+if (nowDate >= xmasStart && nowDate <= xmasEnd) {
+    document.getElementById("santahat").style.display = "block";
+} else {
+    document.getElementById("santahat").style.display = "none";
+}
+
+// var halloweenStart = dateObject.getFullYear() + "-10-07";
+// var halloweenEnd = dateObject.getFullYear() + "-10-31";
+var halloweenStart = dateObject.getFullYear() + "-02-07";
+var halloweenEnd = dateObject.getFullYear() + "-02-31";
+if (nowDate >= halloweenStart && nowDate <= halloweenEnd) {
+    document.getElementById("ghosty").style.display = "block";
+} else {
+    document.getElementById("ghosty").style.display = "none";
+}
+
+
+// Firework loads inanother file "firework.js"
+
+
 // **************************************************************************************************
 // Function to remove an event listener
 function removeEventListener(selector, event, callback) {
