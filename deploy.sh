e31b188b2c55dc6d3ac51d07cf59d4b37f145d27
Angel Crawford <16148683+AngelCrawford@users.noreply.github.com>
2025-10-11 15:10:40 +0200

    Umami Update for cloud - Forgot a link

--- deploy.sh
+++ deploy.sh
@@ -49,4 +49,4 @@ git push origin master --tags
 git subtree push --prefix=public git@github.com:AngelCrawford/profilecard-public.git gh-pages
 
 # Empty the public folder again, after everything is up
-rm -rf public/*
+# rm -rf public/*
